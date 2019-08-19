eef7fcd7254c518ae09865cbdf61796cb4ec689b
Betsi12 <betsi.salas12@gmail.com>
2019-08-19 09:59:48 -0400

    guardando logo de pinterest y subiendo primer componente Nav

---
+++ src/Components/Nav.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component{
+
+    render() {
+      return (
+        <form onSubmit={this.bringdata}>
+          <div className="container-all">
+          <div id="medium">
+          <button id='content-icon'><i className='fab fa-pinterest'></i></button>
+          <div id='cap-input'>
+          <div id='all-input'>
+          <img id="lupa" src="/Imagenes/buscador-musical.png"></img>
+          <input id='buscar' ref={this.busquedaRef} type="search" placeholder="Buscar" ></input>
+          </div>
+          </div>
+          </div>
+          <div id="buttons" className="buttons">
+          <button id="inicio">Inicio</button>
+          <button id="siguiendo">Siguiendo</button>
+          <button id="sarin">S</button>
+          <button id="sara">Sara</button>
+          <i id="shar" className='fas fa-comment-dots'></i>
+          <i id="bell" className='far fa-bell'></i>
+          <i id="points" className='fas fa-ellipsis-h'></i>
+          </div>
+          </div>
+          </form>
+      );
+    }
+  }
+  
+  export default Navbar;
