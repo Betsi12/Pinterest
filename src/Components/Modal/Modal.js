93dfa2a4f1ee1bc222cfad966ebba8c779461926
Betsi12 <betsi.salas12@gmail.com>
2019-08-20 21:52:47 -0400

    subiendo componente modal

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,40 +1,28 @@
 import React from 'react';
-import './Modal.css'
 
-function Modal (props) {
-        return (
-            <div className="container-modal" onClick={(e)=>{
-                if(e.target.getAttribute("class") === "container-modal"){
-                    props.onClose()
-                }
-                }}>
-                <i className="fas fa-angle-left"></i>    
-            <div className="modal">
-                <div className="header-modal">
-                    <i className="fas fa-ellipsis-h"></i>
-                    <button className="btn-send"><i class="fas fa-upload"></i> Enviar</button>
-                    <button className="btn-save"><i class="fas fa-thumbtack"></i> Guardar</button>
-                </div>
+import './modal.css';
 
-                <div className="containerImgyText">
-                    <div className="img-container">
-                        <img className="img-modal" src={props.img} alt=""/>
+const Modal = (props) => {
+    return (
+        <div className="container-modal" onClick={(e)=>{
+            if(e.target.getAttribute("class") === "flex"){
+                props.pushClick()
+            }
+            }}>
+            <a href="/inicio" className="btn-inicio"><i className='fas fa-chevron-left' style={{ fontSize: '20px'}}></i>Inicio</a>
+            <div className="flex">
+                <div className="modal">
+                    <div className="modal-header">
+                        <a href="/guardar" className="btn-guardar"><i className='fas fa-thumbtack' style={{ fontSize: '15px'}}></i>Guardar</a>
+                        <a href="/enviar" className="btn-enviar"><i className='fas fa-upload' style={{ fontSize: '15px'}}></i>Enviar</a>
                     </div>
-                    <div className="text-container">
-                        <h5>{props.phototitle}</h5>
-                        <button className="link">yourphoto.com</button>
-                        <p className="paragraph">Hope you like this photo, if you do...put a like.</p>
-                        <p className="paragraph">Publicado el 19 de Agosto de 2019</p>
-                        <p className="paragraph">{props.category}</p>
-                        <hr className="line"/>
-                        <p className="text">Fotos y comentarios</p>
-                        <button className="btn-numberphotos"> 20 Fotos</button>
-                        <p className="coments">Comentarios</p>
+                    <div className="modal-body">
+                        <img className="img-modal" src={props.imgUrl} alt=""/>
                     </div>
                 </div>
             </div>
-            </div>
-        )
+        </div>
+    )
 }
 
 export default Modal;
